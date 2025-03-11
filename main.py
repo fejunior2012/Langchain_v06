42871424e2959c1f4eab6359172fcad5071b4d81
fejunior2012 <45799110+fejunior2012@users.noreply.github.com>
2025-03-10 22:37:57 -0300

    Update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
 from transformers import GPT2TokenizerFast
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -14,6 +15,12 @@ os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 # Carrega a chave OpenAI
 load_dotenv()
 
+# Definindo se vai criar o banco de dados (True) ou usar um existente (False)
+criar_bd = False  # Mude para False quando quiser usar o banco existente
+
+# Caminho do banco de dados
+db_path = "faiss_db"  # Caminho para salvar o banco de dados FAISS
+
 # Carrega o PDF
 loader = PyPDFLoader("manual_crosser150sabs_2024.pdf")
 pages = loader.load()
@@ -27,17 +34,33 @@ def count_tokens(text: str) -> int:
 
 # Divide o texto em pedaços menores
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=512,
-    chunk_overlap=24,
+    # chunk_size=512,
+    chunk_size=256,
+    chunk_overlap=12,
     length_function=count_tokens,
 )
 
 chunks = text_splitter.create_documents([text])
 
-# Gera embeddings e armazena no FAISS
-print(os.environ["OPENAI_API_KEY"])
+# Gera embeddings
 embeddings = OpenAIEmbeddings(openai_api_key=os.environ["OPENAI_API_KEY"], model="text-embedding-ada-002")
-db = FAISS.from_documents(chunks, embeddings)
+
+# Se a variável `criar_bd` for True, cria o banco de dados FAISS
+if criar_bd:
+    db = FAISS.from_documents(chunks, embeddings)
+    
+    db.save_local(db_path)  # Salva o banco de dados localmente
+    print(f"Banco de dados criado e salvo em {db_path}")
+else:
+    # Caso contrário, carrega o banco de dados FAISS existente
+    # Carregando o banco de dados FAISS, permitindo deserialização "perigosa" se for seguro
+    try:
+        db = FAISS.load_local(db_path, embeddings, allow_dangerous_deserialization=True)
+        print(f"Banco de dados carregado de {db_path}")
+    except Exception as e:
+        print(f"Erro ao carregar o banco de dados: {str(e)}")
+
+    print(f"Banco de dados carregado de {db_path}")
 
 # Criando o contexto para a pergunta
 contexto = """
@@ -47,22 +70,38 @@ Use esse contexto para responder às perguntas de forma clara, precisa e direta.
 """
 
 # Faz a pesquisa semântica
-query = "Qual é óleo do motor?"
-docs = db.similarity_search(query)
+def generate_response(query):
+    docs = db.similarity_search(query, k=5) 
 
-# Verifica se há documentos retornados antes de continuar
-if docs:
-    print(f"Documento mais relevante:\n{docs[0]}")
+    # Verifica se há documentos retornados antes de continuar
+    if docs:
+        print(f"Documento mais relevante:\n{docs[0]}")
 
-    # Carrega a chain e executa a resposta
-    chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
-        # Invocando a chain com contexto
-    
-    response = chain.invoke({
-        "input_documents": docs,
-        "question": f"{contexto}\nPergunta: {query}"
-    })
+        # Carrega a chain e executa a resposta
+        chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
+            # Invocando a chain com contexto
+        
+        response = chain.invoke({
+            "input_documents": docs,
+            "question": f"{contexto}\nPergunta: {query}"
+        })
 
-    print("Resposta:", response["output_text"])  # A resposta fica na chave 'output_text'
-else:
-    print("Nenhum documento encontrado para responder à pergunta.")
+        return response["output_text"]  # A resposta fica na chave 'output_text'
+    else:
+        return "Nenhum documento encontrado para responder à pergunta."
+
+
+def main():
+    st.set_page_config(
+        page_icon="E-mail manager"
+    )
+    st.header("Documento PDF")
+    query = st.text_area("Pergunte ao documento?")
+
+    if query:
+        st.write("Buscando informações dentro do documento...")        
+        result = generate_response(query)
+        st.info(result)
+
+if __name__ == '__main__':
+    main()
